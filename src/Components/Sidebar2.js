3bcf3f0fea2b64c961a81bf5a61a7cace0486758
Abhishek Shukla <90856267+abhishek2002Git@users.noreply.github.com>
2022-07-30 16:35:46 +0530

    sidebar complete

--- src/Components/Sidebar2.js
+++ src/Components/Sidebar2.js
@@ -3,17 +3,45 @@ import { AppState } from "../contexts/Context";
 import "./sidebar2.css";
 
 const Sidebar2 = () => {
-    const {sidebar, showSidebar} = AppState();
+  const { sidebar, showSidebar } = AppState();
+  const [subItemAbout, setSubItemAbout] = useState(false);
+  const showSubItemAbout = () => {
+    setSubItemAbout(!subItemAbout);
+  };
 
   return (
     <>
       <div id="container">
-        <div id="lgMenu" className={sidebar?"enter":""}>
-          <span onClick={showSidebar} id="exit">&times;</span>
+        <div
+          id="lgMenu"
+          className={sidebar ? "enter" : ""}
+          style={{ boxShadow: sidebar ? "5px 5px 15px grey" : "" }}
+        >
+          <span onClick={showSidebar} id="exit">
+            &times;
+          </span>
           <ul className="side-ul">
-            <li className="side-li">Link One</li>
-            <li className="side-li">Link Two</li>
-            <li className="side-li">Link Three</li>
+            <li className="side-li">DARK</li>
+            <li onClick={showSubItemAbout} className="side-li">
+              ABOUT ME <i class="arrow down"></i>
+            </li>
+            <li
+              style={{ display: subItemAbout ? "" : "none" }}
+              className="side-li sub-item-about"
+            >
+              <i class="arrow right"></i>PORTFOLIO
+            </li>
+            <li
+              style={{ display: subItemAbout ? "" : "none" }}
+              className="side-li sub-item-about"
+            >
+              <i class="arrow right"></i>CONTACT
+            </li>
+            <li className="side-li">BLOG</li>
+            <li className="side-li">REACTJS</li>
+            <li className="side-li">CSS</li>
+            <li className="side-li">JAVASCRIPT</li>
+            <li className="side-li">BOOKMARKS</li>
           </ul>
         </div>
       </div>
